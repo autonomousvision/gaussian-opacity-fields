3ecea9d594dcdd0255779a01440ee0dc65e92602
niujinshuchong <yuzh@shanghaitech.edu.cn>
2024-05-01 16:05:45 +0000

    remove unsed code in tetra-triangulation

--- submodules/tetra-triangulation/src/triangulation.cpp
+++ submodules/tetra-triangulation/src/triangulation.cpp
@@ -23,12 +23,6 @@ typedef CGAL::Triangulation_vertex_base_with_info_3<unsigned int, K> Vb;
 typedef CGAL::Triangulation_data_structure_3<Vb> Tds;
 typedef CGAL::Delaunay_triangulation_3<K, Tds> Triangulation;
 
-typedef K::FT FT;
-typedef CGAL::Parallel_if_available_tag Concurrency_tag;
-
-typedef Triangulation::Cell_handle Cell_handle;
-typedef Triangulation::Vertex_handle Vertex_handle;
-typedef Triangulation::Locate_type Locate_type;
 typedef Triangulation::Point Point;
 
 std::vector<uint4> triangulate(size_t num_points, float3* points) {
@@ -72,63 +66,4 @@ std::vector<uint4> triangulate(size_t num_points, float3* points) {
     //                                                   0, max_depth);
     
     return cells;
-}
-
-struct OctreeNode {
-    std::array<std::shared_ptr<OctreeNode>, 8> children;
-    std::vector<std::pair<Point, unsigned int>> points;
-    // Other fields for octree structure
-};
-
-std::shared_ptr<OctreeNode> build_octree(const std::vector<std::pair<Point, unsigned int>>& points, float min_x, float max_x,
-                         float min_y, float max_y, float min_z, float max_z, int depth, int max_depth) {
-    if (depth == max_depth) {
-        std::shared_ptr<OctreeNode> node;
-        node->points = points;
-        // Code to set other fields for the octree structure
-        return node;
-    }
-
-    float mid_x = (min_x + max_x) / 2.0f;
-    float mid_y = (min_y + max_y) / 2.0f;
-    float mid_z = (min_z + max_z) / 2.0f;
-
-    std::array<std::vector<std::pair<Point, unsigned int>>, 8> child_points;
-    for (const auto& p : points) {
-        int index = ((p.first.x() >= mid_x) << 2) | ((p.first.y() >= mid_y) << 1) | (p.first.z() >= mid_z);
-        child_points[index].push_back(p);
-    }
-
-    std::shared_ptr<OctreeNode> node;
-    for (int i = 0; i < 8; i++) {
-        if (!child_points[i].empty()) {
-            float child_min_x = i & 4 ? mid_x : min_x;
-            float child_max_x = i & 4 ? max_x : mid_x;
-            float child_min_y = i & 2 ? mid_y : min_y;
-            float child_max_y = i & 2 ? max_y : mid_y;
-            float child_min_z = i & 1 ? mid_z : min_z;
-            float child_max_z = i & 1 ? max_z : mid_z;
-            node->children[i] = build_octree(child_points[i], child_min_x, child_max_x,
-                                             child_min_y, child_max_y, child_min_z, child_max_z,
-                                             depth + 1, max_depth);
-        }
-    }
-    return node;
-}
-
-
-
-float find_average_spacing(size_t num_points, float3* points) {
-    const unsigned int nb_neighbors = 6;  // 1 ring
-    std::vector<Point> L(num_points);
-    for (size_t i = 0; i < num_points; i++) {
-        const auto p = Point(
-            points[i].x,
-            points[i].y,
-            points[i].z);
-        L[i] = p;
-    }
-
-    FT average_spacing = CGAL::compute_average_spacing<Concurrency_tag>(L, nb_neighbors);
-    return average_spacing;
 }
--- submodules/tetra-triangulation/src/triangulation.h
+++ submodules/tetra-triangulation/src/triangulation.h
@@ -3,4 +3,3 @@
 #include "utils/exception.h"
 
 std::vector<uint4> triangulate(size_t num_points, float3 *points);
-float find_average_spacing(size_t num_points, float3 *points);
